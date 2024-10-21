32616c2d8b43b339217efa4e0fd55ad4e3c4f3b1
Srishti Kakria <srishtikakria@Srishtis-Laptop.bbrouter>
2024-10-21 12:22:17 +0530

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+
+export const Footer = () => {
+  return (
+      <footer className="p-4 bg-white shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
+
+        
+      </footer>
+  )
+}
---
+++ src/hooks/useFetch.js
@@ -0,0 +1,17 @@
+import { useState, useEffect } from "react";
+
+export const useFetch = (apiPath, queryTerm="") => {
+    const [data, setData] = useState([]);
+    const url = `https://api.themoviedb.org/3/${apiPath}?api_key=2a8e4ece79408e0ba6e8ca8dc504e5ac&query=${queryTerm}`
+
+    useEffect(() => {
+        async function fetchMovies(){
+          const response = await fetch(url);
+          const json = await response.json();
+          setData(json.results);
+        }
+        fetchMovies();
+      }, [url])
+
+  return { data }
+}
